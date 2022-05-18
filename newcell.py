bdf35c95ba2b21f8e140b04c55ab80ae5cae9477
retral <mahdialebrahim299@gmail.com>
2022-05-18 11:23:15 +0430

    Done!

--- newcell.py
+++ newcell.py
@@ -5,19 +5,52 @@ import numpy as np
 import os
 import shutil
 import sys
+import pyfiglet
 
 
+R = '\033[31m'
+G = '\033[32m'
+C = '\033[36m'
+W = '\033[0m'
 
+splitter = '='*50
 
-df_new_CC2 = pd.read_excel('new_cell.xlsx', sheet_name='CC2_Dialy')
-df_new_CC3 = pd.read_excel('new_cell.xlsx', sheet_name='CC3_Dialy')
-df_new_RD2 = pd.read_excel('new_cell.xlsx', sheet_name='RD2_Dialy')
-df_new_RD3 = pd.read_excel('new_cell.xlsx', sheet_name='RD3_Dialy')
-df_new_RD4 = pd.read_excel('new_cell.xlsx', sheet_name='RD4_Dialy')
+def ban():
+
+    print(f'''{pyfiglet.figlet_format('new_cell')}
+{splitter}''')
+
+def lister():
+
+    print(f'''
+    1.
+    2.
+    3.
+    4.
+    5.
+    6.
+    
+{splitter}''')
+
+df_new_CC2 = pd.read_excel('new_cell.xlsx', sheet_name='CC2_Daily')
+df_new_CC3 = pd.read_excel('new_cell.xlsx', sheet_name='CC3_Daily')
+df_new_RD2 = pd.read_excel('new_cell.xlsx', sheet_name='RD2_Daily')
+df_new_RD3 = pd.read_excel('new_cell.xlsx', sheet_name='RD3_Daily')
+df_new_RD4 = pd.read_excel('new_cell.xlsx', sheet_name='RD4_Daily')
 
 
 
 if __name__ == "__main__":
 
-    pass
+    while True:
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        ban()
+
+        lister()
+
+        userCh = int(input('Enter tech as integer : '))
+
+
     
